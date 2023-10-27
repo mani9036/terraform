c13f29fc77b957a34fbf317d3e51f6f3f1b7d381
mani9036 <147683899+mani9036@users.noreply.github.com>
2023-10-27 16:32:37 +0530

    Delete v1-VPC_with_EC2 directory

--- v1-VPC_with_EC2/output.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip_of__server" {
-    description = "this is the public IP"
-    value = aws_instance.server.public_ip
-}
-
-output "private_ip_of__server" {
-    description = "this is the public IP"
-    value = aws_instance.server.private_ip
-}
